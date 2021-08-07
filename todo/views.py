16822c926c20c8df53a4ffe72b232cf5e3fd641d
manell0 <svante.magnell@gmail.com>
2021-08-07 13:58:33 +0000

    Add toggle and delete functionality

--- todo/views.py
+++ todo/views.py
@@ -41,3 +41,17 @@ def edit_item(request, item_id):
     }
     return render(request, 'todo/edit_item.html', context)
 
+
+# Toggle items
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+# Delete items
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
